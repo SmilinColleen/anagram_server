fa4478a4e75c884808665d17aa88f97fb8cb4346
Apprentice <apprentice@devbootcamp.com>
2013-08-05 18:04:53 -0700

    add .chop function to line, to get rid of newline character

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,18 @@
 class Word < ActiveRecord::Base
-  # Remember to create a migration!
+
+  def anagrams
+
+  end
 end
+
+# Take each letter of passed-in word, and shovel into array
+# Get length/size of array and sort
+# Find words in dictionary with same number of letters
+# Iterate through that list and shovel each character in each word into an array
+# Sort that array
+# Compare initial array to every other array, looking for equality. If equal,
+# then return the original dictionary word. 
+
+
+##1) passed-in-word.downcase.split('').sort
+##1a) length = passed-in-word.split('').length
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,9 @@
 # filename = 'sinatra_skeleton/app/words.txt'
 
 File.open('words', 'r').each_line do |line|
-  Word.create(word: line)
+  Word.create(word: line.chop)
 end
+
+# File.open('words2', 'r').each_line do |line|
+#   Word.create(word: line.chop)
+# end
