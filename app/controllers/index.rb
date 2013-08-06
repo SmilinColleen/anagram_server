aa6679838493f087f54ab329c1a31b735beb2a26
Apprentice <apprentice@devbootcamp.com>
2013-08-05 17:28:20 -0700

    change code in controller/index.rb to display word in URL. Add words file, and build out seeds.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,10 @@
 get '/' do
+  # @word = "No words yet"
   # Look in app/views/index.erb
+  erb :index
+end
+
+get '/:word' do
+  @word = params[:word]
   erb :index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
-require_relative '../app/models/word.rb'
-require_relative '../app/words.txt'
+# require_relative '../app/models/word.rb'
+# require_relative '../app/words.txt'
+
+# filename = 'sinatra_skeleton/app/words.txt'
+
+File.open('words', 'r').each_line do |line|
+  Word.create(word: line)
+end
